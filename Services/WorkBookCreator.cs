698700bdc3616fb7608fc5f73c0cb0e6ded9e3a3
robertovargash <robertovargashdez@gmail.com>
2022-10-17 17:00:47 -0400

    Adding Workbooks and save excel

--- Services/WorkBookCreator.cs
+++ Services/WorkBookCreator.cs
@@ -1,5 +1,7 @@
 ﻿using BlazeDocX.Models;
 using Microsoft.JSInterop;
+using MudBlazor;
+using System.Globalization;
 using Xceed.Words.NET;
 using Xceed.Workbooks.NET;
 
@@ -14,67 +16,93 @@ namespace BlazeDocX.Services
         {
             jsRuntime = _jsRuntime;
         }
-        public async Task CreateWorkbook()
+        public async Task CreateWorkbook(Accounting accounting)
         {
             using (var workbook = Workbook.Create("First"))
             {
                 // Get the first worksheet. A workbook contains at least 1 worksheet.
                 var worksheet = workbook.Worksheets[0];
+                worksheet.Name = "Incomes";
 
                 // Add a title.
-                worksheet.Cells["B1"].Value = "Create a Workbook";
-                worksheet.Cells["B1"].Style.Font = new Font() { Bold = true, Size = 15.5d };
+                worksheet.Cells["A1"].Value = "Incomes";
+                worksheet.Cells["A1"].Style.Font = new Font() { Bold = true, Size = 15.5d };
+                worksheet.Cells["A1"].Style.Font.Color = System.Drawing.Color.Green;
+                
+                worksheet.MergedCells.Add("A1", "D1");
 
-                // Generate stats.
-                var rnd = new Random();
-                var northDivisionTeams = new Dictionary<string, Data>()
-        {
-          { "Montreal Canadiens", GetStats(rnd) },
-          { "Toronto Maple Leafs", GetStats(rnd) },
-          { "Edmonton Oilers", GetStats(rnd) },
-          { "Calgary Flames", GetStats(rnd) },
-          { "Winnipeg Jets", GetStats(rnd) },
-          { "Ottawa Senators", GetStats(rnd) },
-          { "Vancouver Canucks", GetStats(rnd) }
-        };
-
-                // Fill cells values and row styles/size.
-                worksheet.Rows[4].Cells[2].Value = "2021 NHL Standings";
-                worksheet.Rows[4].Style.Font.Bold = true;
-                worksheet.Rows[4].Height = 50;
-
-                worksheet.Rows[5].Cells[0].Value = "North Division";
-                worksheet.Rows[6].Cells[1].Value = "Pts";
-                worksheet.Rows[6].Cells[2].Value = "Wins";
-                worksheet.Rows[6].Cells[3].Value = "%";
-                worksheet.Rows[6].Cells[4].Value = "In Playoffs";
-                worksheet.Rows[6].Cells[5].Value = "Last Win";
-                worksheet.Rows[6].Style.Alignment.Horizontal = HorizontalAlignment.Center;
-
-                var teams = northDivisionTeams.OrderByDescending(entry => entry.Value.Pts);
-                for (int i = 0; i < teams.Count(); ++i)
+                worksheet.Rows[2].Cells[0].Value = "Category";
+                worksheet.Rows[2].Cells[1].Value = "Date";
+                worksheet.Rows[2].Cells[2].Value = "Amount";
+                worksheet.Columns["C"].Style.CustomFormat = "0.00";
+                worksheet.Rows[2].Cells[3].Value = "Details";
+                worksheet.Rows[2].Style.Font.Bold = true;
+                string bcell = worksheet.Rows[2].Cells[2].Address;
+                string endcell = worksheet.Rows[2].Cells[2].Address;
+                for (int i = 1; i <= accounting.Incomes.Count; i++)
                 {
-                    var team = teams.ElementAt(i);
-                    worksheet.Rows[7 + i].Cells[0].Value = team.Key;
-                    worksheet.Rows[7 + i].Cells[1].Value = team.Value.Pts;
-                    worksheet.Rows[7 + i].Cells[2].Value = team.Value.Wins;
-                    worksheet.Rows[7 + i].Cells[3].Value = team.Value.Percent;
-                    worksheet.Rows[7 + i].Cells[4].Value = (i <= 3);
-                    worksheet.Rows[7 + i].Cells[5].Value = team.Value.LastWin;
+                    worksheet.Rows[2 + i].Cells[0].Value = accounting.Incomes[i-1].Category.ToString();
+                    worksheet.Rows[2 + i].Cells[1].Value = ((DateTime)accounting.Incomes[i - 1].Date).ToString("d", DateTimeFormatInfo.CurrentInfo);
+                    worksheet.Rows[2 + i].Cells[2].Value = accounting.Incomes[i - 1].Amount;
+                    worksheet.Rows[2 + i].Cells[3].Value = accounting.Incomes[i - 1].Details;
+                    if (i == accounting.Incomes.Count)
+                    {
+                        endcell = worksheet.Rows[2 + i].Cells[2].Address;
+                    }
                 }
+                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Formula = $"=SUM( {bcell}:{endcell} )";
+                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.Font = new Font() { Bold = true};
+                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.CustomFormat = "0.00";
+                // Generate stats.
+                //var rnd = new Random();
+                //var northDivisionTeams = new Dictionary<string, Data>()
+                //{
+                //      { "Montreal Canadiens", GetStats(rnd) },
+                //      { "Toronto Maple Leafs", GetStats(rnd) },
+                //      { "Edmonton Oilers", GetStats(rnd) },
+                //      { "Calgary Flames", GetStats(rnd) },
+                //      { "Winnipeg Jets", GetStats(rnd) },
+                //      { "Ottawa Senators", GetStats(rnd) },
+                //      { "Vancouver Canucks", GetStats(rnd) }
+                //};
 
-                // Set the style display format for all cells in column "D".
-                worksheet.Columns["D"].Style.CustomFormat = "0.000";
+                //// Fill cells values and row styles/size.
+                //worksheet.Rows[4].Cells[2].Value = "2021 NHL Standings";
+                //worksheet.Rows[4].Style.Font.Bold = true;
+                //worksheet.Rows[4].Height = 50;
 
-                // AutoFit all columns with content, from row 6 to 13, with a minimum width of 0 and a maximum width of 255.
-                worksheet.Columns.AutoFit(0, 255, 6, 13);
+                //worksheet.Rows[5].Cells[0].Value = "North Division";
+                //worksheet.Rows[6].Cells[1].Value = "Pts";
+                //worksheet.Rows[6].Cells[2].Value = "Wins";
+                //worksheet.Rows[6].Cells[3].Value = "%";
+                //worksheet.Rows[6].Cells[4].Value = "In Playoffs";
+                //worksheet.Rows[6].Cells[5].Value = "Last Win";
+                //worksheet.Rows[6].Style.Alignment.Horizontal = HorizontalAlignment.Center;
 
-                // Set Outline and inside borders for CellRange A7 to F14.
-                worksheet.Cells["A7", "F14"].Style.Borders.SetOutline(LineStyle.Medium, System.Drawing.Color.Blue);
-                worksheet.Cells["A7", "F14"].Style.Borders.SetInside(LineStyle.Medium, System.Drawing.Color.Blue);
+                //var teams = northDivisionTeams.OrderByDescending(entry => entry.Value.Pts);
+                //for (int i = 0; i < teams.Count(); ++i)
+                //{
+                //    var team = teams.ElementAt(i);
+                //    worksheet.Rows[7 + i].Cells[0].Value = team.Key;
+                //    worksheet.Rows[7 + i].Cells[1].Value = team.Value.Pts;
+                //    worksheet.Rows[7 + i].Cells[2].Value = team.Value.Wins;
+                //    worksheet.Rows[7 + i].Cells[3].Value = team.Value.Percent;
+                //    worksheet.Rows[7 + i].Cells[4].Value = (i <= 3);
+                //    worksheet.Rows[7 + i].Cells[5].Value = team.Value.LastWin;
+                //}
 
-                // Set Fill for CellRange A7 to F7
-                worksheet.Cells["A7", "F7"].Style.Fill.BackgroundColor = System.Drawing.Color.Orange;
+                //// Set the style display format for all cells in column "D".
+                //worksheet.Columns["D"].Style.CustomFormat = "0.000";
+
+                //// AutoFit all columns with content, from row 6 to 13, with a minimum width of 0 and a maximum width of 255.
+                //worksheet.Columns.AutoFit(0, 255, 6, 13);
+
+                //// Set Outline and inside borders for CellRange A7 to F14.
+                //worksheet.Cells["A7", "F14"].Style.Borders.SetOutline(LineStyle.Medium, System.Drawing.Color.Blue);
+                //worksheet.Cells["A7", "F14"].Style.Borders.SetInside(LineStyle.Medium, System.Drawing.Color.Blue);
+
+                //// Set Fill for CellRange A7 to F7
+                //worksheet.Cells["A7", "F7"].Style.Fill.BackgroundColor = System.Drawing.Color.Orange;
 
                 // Save the created workbook.
                 using MemoryStream memStream = new();
