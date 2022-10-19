be18d6c2248a9d3e3acd174b842e1a3b0a666fb5
robertovargash <robertovargashdez@gmail.com>
2022-10-19 19:20:29 -0400

    ExportExpenses

--- Services/WorkBookCreator.cs
+++ Services/WorkBookCreator.cs
@@ -21,40 +21,10 @@ namespace BlazeDocX.Services
             using (var workbook = Workbook.Create("First"))
             {
                 // Get the first worksheet. A workbook contains at least 1 worksheet.
-                var worksheet = workbook.Worksheets[0];
-                worksheet.Name = "Incomes";
-
-                // Add a title.
-                worksheet.Cells["A1"].Value = "Incomes";
-                worksheet.Cells["A1"].Style.Font = new Font() { Bold = true, Size = 15.5d };
-                worksheet.Cells["A1"].Style.Font.Color = System.Drawing.Color.Green;
-                
-                worksheet.MergedCells.Add("A1", "D1");
-
-                worksheet.Rows[2].Cells[0].Value = "Category";
-                worksheet.Rows[2].Cells[1].Value = "Date";
-                worksheet.Rows[2].Cells[2].Value = "Amount";
-                worksheet.Columns["C"].Style.CustomFormat = "0.00";
-                worksheet.Rows[2].Cells[3].Value = "Details";
-                worksheet.Rows[2].Style.Font.Bold = true;
-                string bcell = worksheet.Rows[2].Cells[2].Address;
-                string endcell = worksheet.Rows[2].Cells[2].Address;
-                for (int i = 1; i <= accounting.Incomes.Count; i++)
-                {
-                    worksheet.Rows[2 + i].Cells[0].Value = accounting.Incomes[i-1].Category.ToString();
-                    worksheet.Rows[2 + i].Cells[1].Value = ((DateTime)accounting.Incomes[i - 1].Date).ToString("d", DateTimeFormatInfo.CurrentInfo);
-                    worksheet.Rows[2 + i].Cells[2].Value = accounting.Incomes[i - 1].Amount;
-                    worksheet.Rows[2 + i].Cells[3].Value = accounting.Incomes[i - 1].Details;
-                    if (i == accounting.Incomes.Count)
-                    {
-                        endcell = worksheet.Rows[2 + i].Cells[2].Address;
-                    }
-                }
-                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Formula = $"=SUM( {bcell}:{endcell} )";
-                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.Font = new Font() { Bold = true};
-                worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.CustomFormat = "0.00";
+                ExportIncome(accounting, workbook);
+                ExportExpense(accounting, workbook);
                 ///COMMENT AND UNCOMMENT THIS FOR ISSUE
-                worksheet.Columns.AutoFit(0, 255, 0, 13);
+                //worksheet.Columns.AutoFit(0, 255, 0, 13);
 
                 // Save the created workbook.
                 using MemoryStream memStream = new();
@@ -62,6 +32,79 @@ namespace BlazeDocX.Services
                 await jsRuntime.InvokeVoidAsync("blazeDocX.downloadStream", memStream.GetBuffer(), $"Test.xlsx");
                 //Console.WriteLine("\tCreated: CreateWorkbook.xlsx\n");
             }
+        }
+
+        private static void ExportIncome(Accounting accounting, Workbook workbook)
+        {
+            var worksheet = workbook.Worksheets[0];
+            worksheet.Name = "Incomes";
+
+            // Add a title.
+            worksheet.Cells["A1"].Value = "Incomes";
+            worksheet.Cells["A1"].Style.Font = new Font() { Bold = true, Size = 15.5d };
+            worksheet.Cells["A1"].Style.Font.Color = System.Drawing.Color.Green;
+
+            worksheet.MergedCells.Add("A1", "D1");
+
+            worksheet.Rows[2].Cells[0].Value = "Category";
+            worksheet.Rows[2].Cells[1].Value = "Date";
+            worksheet.Rows[2].Cells[2].Value = "Amount";
+            worksheet.Columns["C"].Style.CustomFormat = "0.00";
+            worksheet.Rows[2].Cells[3].Value = "Details";
+            worksheet.Rows[2].Style.Font.Bold = true;
+            string bcell = worksheet.Rows[2].Cells[2].Address;
+            string endcell = worksheet.Rows[2].Cells[2].Address;
+            for (int i = 1; i <= accounting.Incomes.Count; i++)
+            {
+                worksheet.Rows[2 + i].Cells[0].Value = accounting.Incomes[i - 1].Category.ToString();
+                worksheet.Rows[2 + i].Cells[1].Value = ((DateTime)accounting.Incomes[i - 1].Date).ToString("d", DateTimeFormatInfo.CurrentInfo);
+                worksheet.Rows[2 + i].Cells[2].Value = accounting.Incomes[i - 1].Amount;
+                worksheet.Rows[2 + i].Cells[3].Value = accounting.Incomes[i - 1].Details;
+                if (i == accounting.Incomes.Count)
+                {
+                    endcell = worksheet.Rows[2 + i].Cells[2].Address;
+                }
+            }
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Formula = $"=SUM( {bcell}:{endcell} )";
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.Font = new Font() { Bold = true };
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.CustomFormat = "0.00";
+        }
+
+        private static void ExportExpense(Accounting accounting, Workbook workbook)
+        {
+            workbook.Worksheets.Add();
+            var worksheet = workbook.Worksheets[1];
+            worksheet.Name = "Expenses";
+
+            // Add a title.
+            worksheet.Cells["A1"].Value = "Expenses";
+            worksheet.Cells["A1"].Style.Font = new Font() { Bold = true, Size = 15.5d };
+            worksheet.Cells["A1"].Style.Font.Color = System.Drawing.Color.Red;
+
+            worksheet.MergedCells.Add("A1", "D1");
+
+            worksheet.Rows[2].Cells[0].Value = "Category";
+            worksheet.Rows[2].Cells[1].Value = "Date";
+            worksheet.Rows[2].Cells[2].Value = "Amount";
+            worksheet.Columns["C"].Style.CustomFormat = "0.00";
+            worksheet.Rows[2].Cells[3].Value = "Details";
+            worksheet.Rows[2].Style.Font.Bold = true;
+            string bcell = worksheet.Rows[2].Cells[2].Address;
+            string endcell = worksheet.Rows[2].Cells[2].Address;
+            for (int i = 1; i <= accounting.Expenses.Count; i++)
+            {
+                worksheet.Rows[2 + i].Cells[0].Value = accounting.Expenses[i - 1].Category.ToString();
+                worksheet.Rows[2 + i].Cells[1].Value = ((DateTime)accounting.Expenses[i - 1].Date).ToString("d", DateTimeFormatInfo.CurrentInfo);
+                worksheet.Rows[2 + i].Cells[2].Value = accounting.Expenses[i - 1].Amount;
+                worksheet.Rows[2 + i].Cells[3].Value = accounting.Expenses[i - 1].Details;
+                if (i == accounting.Expenses.Count)
+                {
+                    endcell = worksheet.Rows[2 + i].Cells[2].Address;
+                }
+            }
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Formula = $"=SUM( {bcell}:{endcell} )";
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.Font = new Font() { Bold = true };
+            worksheet.Rows[2 + accounting.Incomes.Count + 1].Cells[2].Style.CustomFormat = "0.00";
         }
 
         #region Private Methods
